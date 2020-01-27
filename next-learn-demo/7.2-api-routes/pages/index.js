7e9b6d1b872061ab46f7c9b6487b4e2740989272
Vincent Nguyen <vpvnguyen@gmail.com>
2020-01-27 13:52:50 -0800

    completed; created API routes and query by condition routes

--- next-learn-demo/7.2-api-routes/pages/index.js
+++ next-learn-demo/7.2-api-routes/pages/index.js
@@ -1,8 +1,32 @@
+import { useRouter } from 'next/router';
+
+// to fetch our API, it's a React Hook for remote data fetching
+import useSWR from 'swr';
+
+function fetcher(url) {
+  return fetch(url).then(r => r.json());
+}
+
 export default function Index() {
+
+  // We're sending the author from the query object received by the index page (/) to the API route, which will then return the filtered quotes. http://localhost:3000/?author=rauch
+  const { query } = useRouter();
+  const { data, error } = useSWR(
+    `/api/randomQuote${query.author ? '?author=' + query.author : ''}`,
+    fetcher
+  );
+  // The following line has optional chaining, added in Next.js v9.1.5,
+  // is the same as `data && data.author`
+  const author = data?.author;
+  let quote = data?.quote;
+
+  if (!data) quote = 'Loading...';
+  if (error) quote = 'Failed to fetch the quote.';
+
   return (
     <main className="center">
-      <div className="quote">Write tests, not too many, mostly integration</div>
-      <span className="author"> - Guillermo Rauch </span>
+      <div className="quote">{quote}</div>
+      {author && <span className="author">- {author}</span>}
 
       <style jsx>{`
         main {
@@ -24,5 +48,5 @@ export default function Index() {
         }
       `}</style>
     </main>
-  )
+  );
 }
