269f7c327f562b1e0a5ab7964104c11c0a6f2c59
Vincent Nguyen <vpvnguyen@gmail.com>
2020-01-27 11:16:28 -0800

    completed data fetching client side using getInitialProps method

--- next-learn-demo/6-fetching-data/pages/p/[id].js
+++ next-learn-demo/6-fetching-data/pages/p/[id].js
@@ -1,13 +1,38 @@
-import { useRouter } from 'next/router'
-import Layout from '../../components/MyLayout.js'
+// import { useRouter } from 'next/router'
+// import Layout from '../../components/MyLayout.js'
 
-export default function Post() {
-  const router = useRouter()
+// export default function Post() {
+//   const router = useRouter()
 
-  return (
-    <Layout>
-      <h1>{router.query.id}</h1>
-      <p>This is the blog post content.</p>
-    </Layout>
-  )
-}
+//   return (
+//     <Layout>
+//       <h1>{router.query.id}</h1>
+//       <p>This is the blog post content.</p>
+//     </Layout>
+//   )
+// }
+
+import Layout from '../../components/MyLayout';
+import fetch from 'isomorphic-unfetch';
+
+const Post = props => (
+  <Layout>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+    {props.show.image ? <img src={props.show.image.medium} /> : null}
+  </Layout>
+);
+
+// The first argument of the function is the context object. It has a query object that we can use to fetch information.
+// In our example, we picked the show ID from query and used it to fetch the show data from the TVMaze API.
+Post.getInitialProps = async function (context) {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  console.log(`Fetched show: ${show.name}`);
+
+  return { show };
+};
+
+export default Post;
