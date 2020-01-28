2dfb031d3078358187e787d481098c118caea1f3
Vincent Nguyen <vpvnguyen@gmail.com>
2020-01-28 11:54:35 -0800

    completed; lazy loading modules using import await over require

--- next-learn-demo/E2-lazy-loading-modules/lib/load-db.js
+++ next-learn-demo/E2-lazy-loading-modules/lib/load-db.js
@@ -1,7 +1,11 @@
 export default async function loadDb() {
-  const firebase = require('firebase/app')
-  require('firebase/database')
+  // Here, we use import() function to load the firebase modules. It returns a promise and we use await to wait and resolve the modules.
+  // const firebase = require('firebase/app')
+  // require('firebase/database')
 
+  const firebase = await import('firebase/app');
+  await import('firebase/database');
+  
   try {
     firebase.initializeApp({
       databaseURL: 'https://hacker-news.firebaseio.com'
