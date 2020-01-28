0debdaedfac871fd873766c8c52cdcc316b1af3b
Vincent Nguyen <vpvnguyen@gmail.com>
2020-01-28 12:38:19 -0800

    completed; created dynamic and lazy loading components using 'next/dynamic'

--- next-learn-demo/E3-lazy-loading-components/lib/with-post.js
+++ next-learn-demo/E3-lazy-loading-components/lib/with-post.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import MyLayout from '../components/MyLayout'
 import marked from 'marked'
-import Highlight from 'react-highlight'
+
+// converting Highlight into a dynamic component using 'next/dynamic'
+// import Highlight from 'react-highlight'
+
+import dynamic from 'next/dynamic';
+
+const Highlight = dynamic(() => import('react-highlight'));
 
 marked.setOptions({
   gfm: true,
@@ -9,17 +15,32 @@ marked.setOptions({
   breaks: true
 })
 
-export default function withPost(options) {
+// modified WithPost HOC which uses the Highlight component only when needed.
+
+export default function WithPost(options) {
   return class PostPage extends React.Component {
+    renderMarkdown() {
+      // If a code snippet contains in the markdown content
+      // then use Highlight component
+      if (/~~~/.test(options.content)) {
+        return (
+          <div>
+            <Highlight innerHTML>{marked(options.content)}</Highlight>
+          </div>
+        );
+      }
+
+      // If not, simply render the generated HTML from markdown
+      return <div dangerouslySetInnerHTML={{ __html: marked(options.content) }} />;
+    }
+
     render() {
       return (
         <MyLayout>
           <h1>{options.title}</h1>
-          <div>
-            <Highlight innerHTML>{marked(options.content)}</Highlight>
-          </div>
+          {this.renderMarkdown()}
         </MyLayout>
-      )
+      );
     }
-  }
+  };
 }
