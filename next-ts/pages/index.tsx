d6c0d017847d9f31e6df97117629793ec0d916a6
Vincent Nguyen <vpvnguyen@gmail.com>
2020-01-28 09:26:06 -0800

    completed typescript next support

---
+++ next-ts/pages/index.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next';
+
+// change to use strict / typescript
+// const Home = ({ userAgent }) => <h1>Heyo, {userAgent}</h1>;
+const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
+    <h1>{userAgent}</h1>
+);
+
+Home.getInitialProps = async ({ req }) => {
+    const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
+    return { userAgent };
+};
+
+export default Home;
