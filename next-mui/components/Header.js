73435a2e000981f66b3e13e4e23355a3ba9e32da
Vincent Nguyen <vpvnguyen@gmail.com>
2020-11-26 16:21:09 -0800

    initial next app with material ui core, styles, and icons

---
+++ next-mui/components/Header.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+const Header = () => (
+  <Head>
+    <title>Create Next App</title>
+    <link rel="icon" href="/favicon.ico" />
+  </Head>
+);
+
+export default Header;
---
+++ next-mui/pages/index.js
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+import PersistentDrawer from "../components/PersistentDrawer";
+
+export default function Index() {
+  return (
+    <>
+      <Header />
+      <PersistentDrawer />
+    </>
+  );
+}
